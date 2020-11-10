c8ca2f6e30d947a2305888901ccddf950510a485
c0ffeeartc <c0ffeeartc@gmail.com>
2020-11-10 20:38:33 +0200

    *! catch exceptions for mac in performance tests. whitespace

--- Tests/Tests_Performance/Program.cs
+++ Tests/Tests_Performance/Program.cs
@@ -8,46 +8,54 @@ namespace Tests
 {
 internal class Program
 {
-  private static Process currentProcess;
+	private static Process currentProcess;
 
-  public static void Main(string[] args)
+	public static void Main(string[] args)
 	{
 		Console.WriteLine("Running performance tests...");
-    currentProcess = Process.GetCurrentProcess();
-    Console.WriteLine($"memory at process start: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
+		currentProcess = Process.GetCurrentProcess();
+		Console.WriteLine($"memory at process start: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
     
 		Lookup_ScopeManager.RegisterAll();
 
 		Thread.Sleep(1500); // why?
 
-    GCSettings.LatencyMode = GCLatencyMode.LowLatency;
-    if (!GC.TryStartNoGCRegion(0)) {
-      throw new Exception("can't start NoGCRegion");
-    }
-    
-    Console.WriteLine($"memory before tests: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
+		GCSettings.LatencyMode = GCLatencyMode.LowLatency;
+		try
+		{
+			if (!GC.TryStartNoGCRegion(0))
+			{
+				Console.WriteLine("Can't start NoGCRegion");
+			}
+		}
+		catch ( Exception ex )
+		{
+			Console.WriteLine("can't start NoGCRegion. " + ex.Message );
+		}
 
-    // 1 exclude component-data-init time from test
+		Console.WriteLine($"memory before tests: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
+
+		// 1 exclude component-data-init time from test
 		run<Entity_AddRemove_CompData_Class>();
-    // 2 include component-data-init into test
-    // (results can be compared with tests 3 4 5 and 11)
+		// 2 include component-data-init into test
+		// (results can be compared with tests 3 4 5 and 11)
 		run<Entity_AddRemove_CompData_Class_AddNewInstance>();
-    // 3 replace component data init by Cache.I.Set
-    // (results can be compared with tests 2 4 5 and 11)
+		// 3 replace component data init by Cache.I.Set
+		// (results can be compared with tests 2 4 5 and 11)
 		run<Entity_AddRemove_CompData_Class_SetUsingCache>();
-    // 4 Create-Apply workflow
-    // (results can be compared with tests 2, 3 5  and 11)
+		// 4 Create-Apply workflow
+		// (results can be compared with tests 2, 3 5  and 11)
 		run<Entity_AddRemove_CompData_Class_CreateSetApply>();
-    // 5 InitSet is a syntax-variant of Create-Apply
-    // (results can be compared with tests 2, 3 4 and 11 but especially with 4)
-    run<Entity_AddRemove_CompData_Class_InitSet>();
-    // 3.2 repeat test#3 to double check stats accuracy
-    run<Entity_AddRemove_CompData_Class_SetUsingCache>();
+		// 5 InitSet is a syntax-variant of Create-Apply
+		// (results can be compared with tests 2, 3 4 and 11 but especially with 4)
+		run<Entity_AddRemove_CompData_Class_InitSet>();
+		// 3.2 repeat test#3 to double check stats accuracy
+		run<Entity_AddRemove_CompData_Class_SetUsingCache>();
     
-    // 10 base struct Add test
+		// 10 base struct Add test
 		run<Entity_AddRemove_CompData_Struct>();
-    // 11 is like 10 but component initialization is included into test
-    // so results can be compared with tests 2-5
+		// 11 is like 10 but component initialization is included into test
+		// so results can be compared with tests 2-5
 		run<Entity_AddRemove_CompData_Struct_IncludeInit>();
 		run<Entity_AddRemove_CompData_Class_WithGroups>();
 		run<Entity_AddRemove_CompData_Struct_WithGroups>();
@@ -65,22 +73,29 @@ internal class Program
 
 		run<Struct_ToString_Implemented>(  );
 		run<Struct_ToString_NotImplemented>(  );
-		run<Struct_ToString_ToGenericTypeString>(  );    Console.WriteLine($"memory after tests: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
+		run<Struct_ToString_ToGenericTypeString>(  );
+
+		Console.WriteLine($"memory after tests: {MemoryHelper.GetMemoryAllStatsString(currentProcess)}");
     
-    try {
-      GC.EndNoGCRegion();
-    } catch (System.InvalidOperationException e) {
-      Console.WriteLine("\nMemory validation needs to be improved... " +
-                        "\nProcess had to clean memory while in NoGC mode" +
-                        "\nNegative memory diffs indicate that");
-      throw;
-    }
+		try
+		{
+  			GC.EndNoGCRegion();
+		}
+		catch (System.InvalidOperationException e)
+		{
+  			Console.WriteLine("\nMemory validation needs to be improved... " +
+				"\nProcess had to clean memory while in NoGC mode" +
+				"\nNegative memory diffs indicate that");
+			throw;
+		}
+		catch ( Exception ex )
+		{
+			Console.WriteLine("Can't start EndNoGCRegion. " + ex.Message );
+		}
 
-    // Console.WriteLine("\nPress any key...");
-    // Console.Read();
-  }
-
-  //Running performance tests...
+		// Console.WriteLine("\nPress any key...");
+		// Console.Read();
+	}
 
 	static void run<T>() where T : IPerformanceTest, new()
 	{
@@ -94,10 +109,11 @@ internal class Program
 		Console.Write( test.Iterations.ToString( "e0" ) + " " + (typeof(T) + ": ").PadRight(60));
 		// For more reliable results, run before
 		PerformanceTestRunner.Run(test);
+
 		Tuple<long,long> msAndMemory = PerformanceTestRunner.Run(new T());
-    Console.Write((msAndMemory.Item1 + " ms").PadRight(10));
-    Console.Write(("mem.DIFF " +msAndMemory.Item2 + " bytes").PadRight(30));
-    Console.WriteLine("mem.ALL " + MemoryHelper.GetMemoryAllStatsString(currentProcess) + " bytes");
+		Console.Write((msAndMemory.Item1 + " ms").PadRight(10));
+		Console.Write(("mem.DIFF " +msAndMemory.Item2 + " bytes").PadRight(30));
+		Console.WriteLine("mem.ALL " + MemoryHelper.GetMemoryAllStatsString(currentProcess) + " bytes");
 	}
 }
 }
