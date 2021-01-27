f94d5fc914f82bce17877adf1d5611f28fa25f0b
c0ffeeartc <c0ffeeartc@gmail.com>
2021-01-28 01:41:23 +0200

    EventsFeature2 OnSelf, OnSelf_Flag Sub helper methods

--- Entitas.Generic.Events/Events2/OnSelf_Flag.cs
+++ Entitas.Generic.Events/Events2/OnSelf_Flag.cs
@@ -8,17 +8,38 @@ public sealed class OnSelf_Flag<TScope,TComp> : OnSelf_Base<TScope>, IEventsFeat
 {
 	public					OnSelf_Flag				( Contexts db ) : base ( db ) { }
 	public static IEventsFeature2_OnSelf_Flag<TScope,TComp> I;
+
+	public					void					Sub						( Entity<TScope> ent, Action<Entity<TScope>> action, bool triggerAfterSub )
+	{
+		Sub( ent.creationIndex, action );
+		if ( triggerAfterSub )
+		{
+			action.Invoke( ent );
+		}
+	}
+
+	public					void					Sub						( Entity<TScope> ent, Action<Entity<TScope>> action, Context<Entity<TScope>> context, bool triggerAfterSub = false )
+	{
+		Sub( ent.creationIndex, action, context );
+		if ( triggerAfterSub )
+		{
+			action.Invoke( ent );
+		}
+	}
 }
 
 public interface IEventsFeature2_OnSelf_Flag<TScope,TComp>
 		where TScope : IScope
 		where TComp : IComponent, ICompFlag, IEvent_Self<TScope,TComp>, Scope<TScope>
 {
+	void					Sub						( Entity<TScope> ent, Action<Entity<TScope>> action, Context<Entity<TScope>> context, bool triggerAfterSub = false );
+	void					Sub						( Entity<TScope> ent, Action<Entity<TScope>> action, bool triggerAfterSub = false );
 	void					Sub						( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
-	void					Unsub					( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
-	void					Invoke					( Int32 id, Entity<TScope> entity, Context<Entity<TScope>> context );
-
 	void					Sub						( Int32 id, Action<Entity<TScope>> action );
+
+	void					Unsub					( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
 	void					Unsub					( Int32 id, Action<Entity<TScope>> action );
+
+	void					Invoke					( Int32 id, Entity<TScope> entity, Context<Entity<TScope>> context );
 }
 }
--- Entitas.Generic.Events/Events2/OnSelf_Removed.cs
+++ Entitas.Generic.Events/Events2/OnSelf_Removed.cs
@@ -15,10 +15,11 @@ public interface IEventsFeature2_OnSelf_Removed<TScope, TComp>
 		where TComp : IComponent, ICompData, IEvent_SelfRemoved<TScope, TComp>, Scope<TScope>
 {
 	void					Sub						( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
-	void					Unsub					( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
-	void					Invoke					( Int32 id, Entity<TScope> entity, Context<Entity<TScope>> context );
-
 	void					Sub						( Int32 id, Action<Entity<TScope>> action );
+
+	void					Unsub					( Int32 id, Action<Entity<TScope>> action, Context<Entity<TScope>> context );
 	void					Unsub					( Int32 id, Action<Entity<TScope>> action );
+
+	void					Invoke					( Int32 id, Entity<TScope> entity, Context<Entity<TScope>> context );
 }
 }
